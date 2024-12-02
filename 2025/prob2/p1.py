70d3456f330ab30bd648198eac9d49c5f0fda15b
jmlopm <che3ma@gmail.com>
2024-12-02 17:28:31 +0100

    Create p1.py

---
+++ 2025/prob2/p1.py
@@ -0,0 +1,34 @@
+
+def increasing(l):
+    for i in range(0, len(l)-1):
+        if l[i+1]<=l[i]:
+            return False
+    return True
+
+def decreasing(l):
+    return increasing(l[::-1])
+
+def noBigDifferences(l):
+    for i in range(0, len(l)-1):
+        if abs(l[i]-l[i+1])>3:
+            return False
+    return True
+    
+
+def isSafe(l):
+    if (increasing(l) or decreasing(l)) and noBigDifferences(l):
+        return True
+    else:
+        return False
+
+def resolver(l):
+    arr=[int(a) for a in l[0:len(l)-1].split(" ")]
+    return isSafe(arr)
+    
+f=open("data")
+ret=0
+for l in f:
+    if resolver(l):
+        ret+=1
+
+print(ret)
